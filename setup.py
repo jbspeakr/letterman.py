eae107ddf99b6af00ac6c9b0c045bcfdf3e1fdaa
jbspeakr <jan@brennenstuhl.me>
2014-06-14 23:11:21 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='letterman.py',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/jbspeakr/letterman.py',
+    license='MIT License',
+    author='Jan Brennenstuhl',
+    author_email='jan@brennenstuhl.me',
+    description='  Python lib for creating DIN 5008 and DIN 676 compliant PDF letters.'
+)
