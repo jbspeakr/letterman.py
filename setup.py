863a97576046a7ff28616b910a93319f04c8bc53
jbspeakr <jan@brennenstuhl.me>
2014-06-16 16:04:15 +0200

    makes setup smarter

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+from letterman import __version__ as package_version
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
 
 setup(
     name='letterman.py',
-    version='0.1',
-    packages=[''],
+    version=package_version,
+    packages=['letterman', 'letterman.test'],
     url='https://github.com/jbspeakr/letterman.py',
-    license='MIT License',
+    license='MIT',
     author='Jan Brennenstuhl',
     author_email='jan@brennenstuhl.me',
-    description='  Python lib for creating DIN 5008 and DIN 676 compliant PDF letters.'
+    description='  Python lib for creating DIN 5008 and DIN 676 compliant PDF letters.',
+    long_description=read('README.md'),
+    install_requires=read('requirements.txt').split('\n'),
 )
